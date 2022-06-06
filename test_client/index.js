914e6a5187ca34a03d62c79fa5f53f2b7fa47bfb
kobayashi-deepinsight <kenta.kobayashi@deepinsight.co.jp>
2022-06-06 20:45:58 +0900

    mapとthree

--- test_client/index.js
+++ test_client/index.js
@@ -1,6 +1,16 @@
 const SERVER = "http://localhost:3000";
 const API_ALBAMLIST = SERVER + "/samples/list";
 
+jQuery.prototype.scrolToCenter = function() {
+    var maxScrollLeft = $(this)[0].scrollWidth - $(this)[0].clientWidth;
+    var maxScrollTop = $(this)[0].scrollHeight - $(this)[0].clientHeight;
+    $(this).scrollLeft(maxScrollLeft / 2);
+    $(this).scrollTop(maxScrollTop / 2);
+}
+
+// ===========================================================================
+// audioPlayer
+// ===========================================================================
 const audioPlayer = {
     player: $("#player"),
     que: [],
@@ -28,9 +38,98 @@ const audioPlayer = {
     },
 
 }
-audioPlayer.init();
 
-Vue.component('albam-template', {
+// ===========================================================================
+// mapControl
+// ===========================================================================
+const mapControl = {
+    init() {
+        $("#map").on("click", function() {
+            $(this).scrolToCenter();
+        });
+
+    },
+    center() {
+        $("#map").scrolToCenter();
+    }
+}
+
+
+// ===========================================================================
+// three
+// ===========================================================================
+import * as THREE from './lib/three.js/three.module.js';
+import { OrbitControls } from './lib/three.js/OrbitControls.js';
+import { GLTFLoader } from './lib/three.js/GLTFLoader.js';
+
+let perpCamera, renderer, mesh, scene, controls;
+
+const three = {
+    init() {
+        var $container = $("#threediv");
+        var render = function render() {
+            renderer.clear();
+            renderer.render(scene, perpCamera);
+        };
+        // scene
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color(0x404040);
+
+        // カメラ
+        perpCamera = new THREE.PerspectiveCamera(45, $container.width() / $container.height(), 1, 2000);
+        perpCamera.position.set(0, 300, 0);
+        perpCamera.lookAt(new THREE.Vector3(0, 0, 0));
+        scene.add(perpCamera);
+
+        // ライト
+        const pointLight = new THREE.PointLight(0xffffff, 1);
+        perpCamera.add(pointLight);
+        const light = new THREE.HemisphereLight(0x888888, 0x532333, 1.2);
+        perpCamera.add(light);
+
+        // モデル
+        var loader = new GLTFLoader().setPath('./images/');
+        loader.load('sedan.glb', function(gltf) {
+            scene.add(gltf.scene);
+            render();
+        });
+
+        // renderer
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize($container.width(), $container.height());
+        $container.append(renderer.domElement);
+
+        // リサイズされたとき
+        var that = this;
+        $(window).on("resize", function() {
+            that.resize();;
+        });
+
+        // OrbitControls
+        controls = new OrbitControls(perpCamera, renderer.domElement);
+        controls.addEventListener('change', render);
+        controls.minDistance = 3;
+        controls.maxDistance = 8;
+        controls.update();
+
+        render();
+    },
+    render() {
+        renderer.clear();
+        renderer.render(scene, perpCamera);
+    },
+    resize() {
+        var $container = $("#threediv");
+        perpCamera.aspect = $container.width() / $container.height();
+        perpCamera.updateProjectionMatrix();
+        renderer.setSize($container.width(), $container.height());
+        this.render();
+    }
+}
+
+// ========== albams ============
+Vue.component('albams', {
     template: `
         <div class="albam" v-on:click="onClick">
             <img v-bind:src=albam.artwork>
@@ -48,8 +147,7 @@ Vue.component('albam-template', {
     }
 });
 
-
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
         view: 1,
@@ -65,4 +163,26 @@ var app = new Vue({
             });
     },
     methods: {},
+    watch: {
+        view: function() {
+            if (3 == this.view) {
+                this.$nextTick(function() {
+                    three.resize();
+                });
+            }
+            if (4 == this.view) {
+                this.$nextTick(function() {
+                    mapControl.center();
+                });
+            }
+        }
+    },
+});
+
+
+// on DOM readed
+$(() => {
+    audioPlayer.init();
+    mapControl.init();
+    three.init();
 });
